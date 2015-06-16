20338edb01d07ca9ceb84405fe834fec10f8fd46
Paul Davis <pauldavis101@gmail.com>
2015-06-16 17:00:14 -0500

    Initial Trash Bag Work

---
+++ src/main/java/com/dyonovan/thingsnstuff/container/SlotNull.java
@@ -0,0 +1,21 @@
+package com.dyonovan.thingsnstuff.container;
+
+import com.dyonovan.thingsnstuff.managers.ItemManager;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+public class SlotNull extends Slot
+{
+    public SlotNull(IInventory iInventory, int i, int i1, int i2)
+    {
+        super(iInventory, i, i1, i2);
+    }
+
+    @Override
+    public IIcon getBackgroundIconIndex()
+    {
+        return ItemManager.trashBag.getIcon(new ItemStack(ItemManager.trashBag, 1), 0);
+    }
+}
---
+++ src/main/java/com/dyonovan/thingsnstuff/gui/GuiTrashBag.java
@@ -0,0 +1,33 @@
+package com.dyonovan.thingsnstuff.gui;
+
+import com.dyonovan.thingsnstuff.container.ContainerTrashBag;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+public class GuiTrashBag extends GuiContainer {
+    private static final ResourceLocation texture = new ResourceLocation("thingsnstuff:textures/gui/trashBagGui.png");
+
+    public GuiTrashBag(InventoryPlayer inventoryPlayer, ItemStack bag) {
+        super(new ContainerTrashBag(inventoryPlayer, bag));
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
+        final String invTitle = "Trash Bag";
+        fontRendererObj.drawString(invTitle, xSize / 2 - fontRendererObj.getStringWidth(invTitle) / 2, 6, 4210752);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+        GL11.glColor4f(1f, 1f, 1f, 1f);
+
+        this.mc.getTextureManager().bindTexture(texture);
+
+        int x = (width - xSize) / 2;
+        int y = (height - ySize) / 2;
+        drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+    }
+}
