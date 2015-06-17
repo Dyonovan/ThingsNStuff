5bb139f8718e5c5a494646560c846227704044f4
Dyonovan <mcbetacraft@gmail.com>
2015-06-17 19:48:58 -0400

    Removed Wire

--- src/main/java/com/dyonovan/thingsnstuff/handlers/BlockHandler.java
+++ src/main/java/com/dyonovan/thingsnstuff/handlers/BlockHandler.java
@@ -2,14 +2,13 @@ package com.dyonovan.thingsnstuff.handlers;
 
 import com.dyonovan.thingsnstuff.ThingsNStuff;
 import com.dyonovan.thingsnstuff.common.blocks.BlockSwitch;
-import com.dyonovan.thingsnstuff.common.blocks.BlockWire;
 import com.dyonovan.thingsnstuff.lib.Constants;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 
 public class BlockHandler {
 
-    public static Block blockSwitch, blockSwitchOn, blockWire;
+    public static Block blockSwitch, blockSwitchOn;
 
     public static void init() {
 
@@ -19,11 +18,8 @@ public class BlockHandler {
         blockSwitchOn = new BlockSwitch(true)
                 .setBlockName(Constants.MODID + ":blockSwitchOn");
 
-        blockWire = new BlockWire();
-
         GameRegistry.registerBlock(blockSwitch, "blockSwitch");
         GameRegistry.registerBlock(blockSwitchOn, "blockSwitchOn");
-        GameRegistry.registerBlock(blockWire, "blockWire");
 
     }
 }
--- src/main/java/com/dyonovan/thingsnstuff/proxy/ClientProxy.java
+++ src/main/java/com/dyonovan/thingsnstuff/proxy/ClientProxy.java
@@ -1,8 +1,6 @@
 package com.dyonovan.thingsnstuff.proxy;
 
 import com.dyonovan.thingsnstuff.handlers.BlockHandler;
-import com.dyonovan.thingsnstuff.client.renderer.ItemRenderBlockWire;
-import com.dyonovan.thingsnstuff.client.renderer.RenderBlockWire;
 import com.dyonovan.thingsnstuff.common.tileentity.TEBlockWire;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -12,11 +10,7 @@ import net.minecraftforge.client.MinecraftForgeClient;
 public class ClientProxy extends CommonProxy {
 
     public void registerRenderer() {
-        //BlockWire
-        TileEntitySpecialRenderer render = new RenderBlockWire();
-        ClientRegistry.bindTileEntitySpecialRenderer(TEBlockWire.class, render);
-        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(BlockHandler.blockWire),
-                new ItemRenderBlockWire(render, new TEBlockWire()));
+
     }
 
     public void registerTESpecialRenderer() {
